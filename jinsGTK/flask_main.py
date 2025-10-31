d4c3df5c104e82692d2d2469049aeffca97ec99c
Juyoung Lee <ejuyoung@gmail.com>
2025-10-31 14:29:54 +0900

    change name

--- jinsGTK/flask_main.py
+++ jinsGTK/flask_main.py
@@ -684,7 +684,7 @@ def runPygame(participant_name, trial_numbers, target_gestures,
         fpsText = fps_font.render("%dfps"%(clock.get_fps()), True, (100,255,100))
         screen.blit(fpsText, (0,0))
     
-    pygame.display.set_caption("Ocular Gesture Toolkit")
+    pygame.display.set_caption("JINS Gesture Toolkit")
     
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
